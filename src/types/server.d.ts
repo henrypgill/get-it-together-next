660b5989648901302454508a36fe6ec234391e12
henrypgill <henrygill3@gmail.com>
2023-09-07 20:51:30 +0100

    Added types for the calendar

---
+++ src/types/server.d.ts
@@ -0,0 +1,44 @@
+
+
+export interface DbEventData {
+    event_id: number;
+    event_host: DbUserData;
+    event_users: DbUserData[];
+    event_dates: EventDate[];
+    event_location: EventLocation;
+}
+
+export interface DbUserData {
+    user_id: number;
+    user_first_name: string;
+    user_last_name: string;
+    user_email: string;
+}
+
+interface EventLocation {
+    name: string;
+    googleMapsUrl?: string;
+}
+
+export interface EventDate {
+    year: EventYear;
+    month: EventMonth;
+    day: EventDay;
+    time: EventTime;
+    users: DbUserData[];
+}
+
+export type EventYear = number;
+
+export interface EventMonth {
+    name: string;
+    index: number;
+    days: number;
+}
+
+export type EventDay = number;
+
+export type EventTime = {
+    hour: number;
+    minute: number;
+}
