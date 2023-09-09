8515be8e68e131e482ea4e03e33f9fe9ffd10d87
henrypgill <henrygill3@gmail.com>
2023-09-09 08:47:19 +0100

    created folder for slices

---
+++ src/redux/slices/calendarSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+    getInitialCalendarState,
+    setCalendarYear,
+    setMonth,
+    stepMonth,
+    stepYear,
+} from "../../app/calendar/calendarUtils";
+// import { CalendarData } from "../components/Calendar/CalendarData";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface CalendarPayload_number {
+    value: number;
+}
+
+
+export const calendarSlice = createSlice({
+    name: "calendar",
+    initialState: getInitialCalendarState(),
+    reducers: {
+        stepYear: {
+            reducer: (state, action: PayloadAction<CalendarPayload_number>) => {
+                stepYear(action.payload.value, state);
+            },
+            prepare: (value: number) => {
+                return { payload: { value } };
+            },
+        },
+        stepMonth: {
+            reducer: (state, action: PayloadAction<CalendarPayload_number>) => {
+                stepMonth(action.payload.value, state);
+            },
+            prepare: (value: number) => {
+                return { payload: { value } };
+            },
+        },
+        setYear: {
+            reducer: (state, action: PayloadAction<CalendarPayload_number>) => {
+                setCalendarYear(action.payload.value, state);
+            },
+            prepare: (value: number) => {
+                return { payload: { value } };
+            },
+        },
+        setMonth: {
+            reducer: (state, action: PayloadAction<CalendarPayload_number>) => {
+                setMonth(action.payload.value, state);
+            },
+            prepare: (value: number) => {
+                return { payload: { value } };
+            },
+        },
+    },
+});
